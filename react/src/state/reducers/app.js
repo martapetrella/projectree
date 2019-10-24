f8c132954f2751358fb5cfa8dc5f3679b6d351ea
MartaPetrella <martollum@gmail.com>
2019-10-24 11:13:33 +0200

    amends

--- react/src/state/reducers/app.js
+++ react/src/state/reducers/app.js
@@ -65,8 +65,8 @@ const initialState = {
             description: 'Press the coffe button and that\'s all',
             type: 'task', // task / bug / enhancment / test / meeting
             deadline: '20200615', // default: business goals deadline. REQUIRED for Meeting event
-            estimate_effort: '2min', // only for task / bug / enhancment / test type
-            real_effort: '2min', // only for task / bug / enhancment / test type
+            estimate_effort: '2min',
+            real_effort: '2min',
             recoursive: 'every day at 9:00 am', // only for test and meeting type
             work_on_it: 0, // 0: pause; 1: I work on it now --> so your boss will know that you are preparing his coffee right now
             owner: 'John Doe', // who will do this task?
